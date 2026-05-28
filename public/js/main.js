ddb4be2250b7a723c1cdd4474e96eede6a84f221
Andres <andres.silva.09@hotmail.com>
2026-05-28 07:42:23 -0300

    Se mejora logica del corrusel. Se  eliminan imagenes extras

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,22 @@
-terval(moverCarrusel, 7000);
+
 const slides = document.querySelector(".slides");
-
-const totalSlides = document.querySelectorAll(".slide").length;
-
+const tarjetas = document.querySelectorAll(".slide");
 let indice = 0;
 
-function moverCarrusel(){
-
+function moverCarrusel() {
     indice++;
 
     slides.style.transition = "transform 1.5s ease";
-
     slides.style.transform = `translateX(-${indice * 100}%)`;
 
-    if(indice === totalSlides - 1){
-
+    if (indice === tarjetas.length - 1) {
         setTimeout(() => {
-
-            slides.style.transition = "none";
-
-            indice = 0;
-
-            slides.style.transform = `translateX(0%)`;
-
-        }, 500);
-
+            slides.style.transition = "none"; 
+            indice = 0;                     
+            slides.style.transform = "translateX(0%)"; 
+        }, 2000); 
     }
-
 }
 
-setInterval(moverCarrusel, 7000)
+
+setInterval(moverCarrusel, 5000);
